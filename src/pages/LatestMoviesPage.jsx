227c906be27c279beb3c9294d434d9068f67b0ee
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-18 15:32:41 +0200

    Added basic pagination for for the 'latest movie' page.

--- src/pages/LatestMoviesPage.jsx
+++ src/pages/LatestMoviesPage.jsx
@@ -1,36 +1,40 @@
+import { useState } from "react"
 import Container from "react-bootstrap/Container"
 import ListGroup from "react-bootstrap/ListGroup"
+import Button from "react-bootstrap/Button"
 import { useQuery } from "react-query"
 import getData from "../services/getData"
-ListGroup
 
 const LatestMoviesPage = () => {
-
-    const { data, isLoading, isError, error } = useQuery(['latest-movies'], getData.getLatestMovies)
+    const [page, setPage] = useState(1)
+    const { data, isLoading, isError, error } = useQuery(['latest-movies', page], () => getData.getLatestMovies(page))
 
     console.log(data)
 
     return (
         <Container>
             <h2>Latest Movies</h2>
-            {isLoading && (
-                <p>Data is loading...</p>
-            )}
-            {isError && (
-                <p>An error occured: {error.message}</p>
-            )}
-            {data && (
-                <ListGroup>
-                    {data.data.results.map(movie =>
-                        <ListGroup.Item key={movie.id}>
-                            <h3>{movie.title}</h3>
-                            <p>{movie.release_date}</p>
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={"Poster of movie: " + movie.title} />
-                        </ListGroup.Item>
-                    )}
-                </ListGroup>
-
-            )}
+                {isLoading && (
+                    <p>Data is loading...</p>
+                )}
+                {isError && (
+                    <p>An error occured: {error.message}</p>
+                )}
+                {data && (
+                    <ListGroup>
+                        {data.data.results.map(movie =>
+                            <ListGroup.Item key={movie.id}>
+                                <h3>{movie.title}</h3>
+                                <p>{movie.release_date}</p>
+                                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={"Poster of movie: " + movie.title} />
+                            </ListGroup.Item>
+                        )}
+                    </ListGroup>
+                )}
+                <div>
+                    <Button onClick={() => setPage(page - 1)}>←</Button>
+                    <Button onClick={() => setPage(page + 1)}>→</Button>
+                </div>
         </Container>
     )
 }
--- src/services/getData.js
+++ src/services/getData.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3" 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
-const getLatestMovies = async () => {
-    const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&include_adult=false`)
+const getLatestMovies = async (pageNumber) => {
+    const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&include_adult=false&page=${pageNumber}`)
     return res
 }
 
