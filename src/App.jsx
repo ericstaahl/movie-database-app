5f4a60572e72229f81bfb3dbaf09c02a5c9f04ff
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-18 09:47:06 +0200

    Added routing. Also added dedicated homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+
 function App() {
 
   return (
-    <div className="App">
-      <p>Movie Database App</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    // <div className="App">
+    //   <p>Movie Database App</p>
+    // </div>
   )
 }
 
