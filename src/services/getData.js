0854c6e03c9d9a29e7138d0e9b4172b678fbe446
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-20 12:02:47 +0200

    Fetching without page parameter if pageNumber is falsy.

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,10 +1,18 @@
 import axios from "axios";
-const BASE_URL = "https://api.themoviedb.org/3" 
+const BASE_URL = "https://api.themoviedb.org/3"
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const getLatestMovies = async (pageNumber) => {
-    const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&include_adult=false&page=${pageNumber}`)
-    return res
+    console.log("pageNumber: ", pageNumber)
+    // If pageNumber is truthy, run the below function, otherwise run the function without page as url param.
+    // Did seem to work even though pageNumber was null though
+    if (pageNumber) {
+        const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&include_adult=false&page=${pageNumber}`)
+        return res
+    } else {
+        const res = await axios.get(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&include_adult=false`)
+        return res
+    }
 }
 
 export default {
