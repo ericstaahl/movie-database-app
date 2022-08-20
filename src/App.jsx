b700765e75b40232f94836a2cd4639dc04c11f08
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-20 13:05:08 +0200

    Added routing for individual movies.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Navigation from './components/Navigation'
 import LatestMoviesPage from './pages/LatestMoviesPage';
+import MoviePage from './pages/MoviePage';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/latest" element={<LatestMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
       </Routes>
     </>
   )
