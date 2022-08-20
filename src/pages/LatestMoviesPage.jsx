be5e367063820b64b05ed7f063d4c646fa9a4ad0
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-20 11:27:10 +0200

    Pagination buttons now become inactive when there is no next page or previous page. Also changed styling of these. If there is no movie poster some text is shown instead.

--- src/pages/LatestMoviesPage.jsx
+++ src/pages/LatestMoviesPage.jsx
@@ -11,48 +11,59 @@ const LatestMoviesPage = () => {
     console.log(page)
     const { data, isLoading, isError, error } = useQuery(['latest-movies', page], () => getData.getLatestMovies(page))
 
+    console.log(data)
+
     return (
-        <Container>
-            <h2>Latest Movies</h2>
-            {isLoading && (
-                <p>Data is loading...</p>
-            )}
-            {isError && (
-                <p>An error occured: {error.message}</p>
-            )}
+        <>
+            <Container>
+                <h2>Latest Movies</h2>
+                {isLoading && (
+                    <p>Data is loading...</p>
+                )}
+                {isError && (
+                    <p>An error occured: {error.message}</p>
+                )}
+                {data && (
+                    <ListGroup>
+                        {data.data.results.map(movie =>
+                            <ListGroup.Item key={movie.id}>
+                                <h3>{movie.title}</h3>
+                                <p>{movie.release_date}</p>
+                                {movie.poster_path
+                                    ? <img
+                                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                        alt={"Poster of movie: " + movie.title}
+                                    />
+                                    : <p>No poster available</p>}
+                            </ListGroup.Item>
+                        )}
+                    </ListGroup>
+                )}
+            </Container >
             {data && (
-                <ListGroup>
-                    {data.data.results.map(movie =>
-                        <ListGroup.Item key={movie.id}>
-                            <h3>{movie.title}</h3>
-                            <p>{movie.release_date}</p>
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={"Poster of movie: " + movie.title} />
-                        </ListGroup.Item>
-                    )}
-                </ListGroup>
+                <div className="d-flex justify-content-center sticky-bottom bg-dark">
+                    <Button className="mx-2 my-2" disabled={page <= 1} onClick={() => {
+                        if (page && page != 1) {
+                            page--
+                        } else {
+                            page = 1
+                        }
+                        setSearchParams({ page: page })
+                    }
+                    }>←</Button>
+                    <Button className="mx-2 my-2" disabled={page >= data.data.total_pages} onClick={() => {
+                        if (page) {
+                            page++
+                        } else {
+                            page = 2
+                        }
+                        console.log(page)
+                        setSearchParams({ page: page })
+                    }
+                    }>→</Button>
+                </div>
             )}
-            <div>
-                <Button onClick={() => {
-                    if (page && page != 1) {
-                        page--
-                    } else {
-                        page = 1
-                    }
-                    setSearchParams({ page: page })
-                }
-                }>←</Button>
-                <Button onClick={() => {
-                    if (page) {
-                        page++
-                    } else {
-                        page = 2
-                    }
-                    console.log(page)
-                    setSearchParams({ page: page })
-                }
-                }>→</Button>
-            </div>
-        </Container >
+        </>
     )
 }
 
