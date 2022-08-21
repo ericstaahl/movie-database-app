d3e5ad100da74836bc4c44905a832924f14091c6
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-21 13:04:52 +0200

    Added routing for actor page.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import Navigation from './components/Navigation'
 import LatestMoviesPage from './pages/LatestMoviesPage';
 import MoviePage from './pages/MoviePage';
+import ActorPage from './pages/ActorPage';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/latest" element={<LatestMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/people/:id" element={<ActorPage />} />
       </Routes>
     </>
   )
