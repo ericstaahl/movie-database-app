485e5bb2fbe26b63f791c606c5b930b84f0bd65d
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-22 13:54:16 +0200

    Cleaned up unused imports.

--- src/pages/TopRatedPage.jsx
+++ src/pages/TopRatedPage.jsx
@@ -1,13 +1,17 @@
 import Container from "react-bootstrap/Container"
-import ListGroup from "react-bootstrap/ListGroup"
-import Button from "react-bootstrap/Button"
 import { useQuery } from "react-query"
 import getData from "../services/getData"
-import { useSearchParams, Link } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 import MovieList from "../components/MovieList"
+import Pagination from "../components/Pagination"
 
 const PopularMoviesPage = () => {
     const [searchParams, setSearchParams] = useSearchParams()
+
+    const handleSetSearchParams = (page) => {
+        setSearchParams({ page: page })
+    }
+
     // Get the page param from the url
     let page = searchParams.get("page")
     console.log(page)
@@ -33,30 +37,7 @@ const PopularMoviesPage = () => {
                 )}
             </Container >
             {data && (
-                <div className="d-flex justify-content-center sticky-bottom bg-dark">
-                    <Button className="mx-2 my-2" disabled={page <= 1} onClick={() => {
-                        // If page is truthy and not equal to 1, subtract page by one
-                        if (page && page != 1) {
-                            page--
-                        } else {
-                            // Else, stay on first page
-                            page = 1
-                        }
-                        setSearchParams({ page: page })
-                    }
-                    }>←</Button>
-                    <Button className="mx-2 my-2" disabled={page >= data.data.total_pages} onClick={() => {
-                        if (page) {
-                            page++
-                        } else {
-                            // If page is falsy, you are on the the first page, therefore page is set to 2
-                            page = 2
-                        }
-                        // Setting the search params so that the url is saved to browser history.
-                        setSearchParams({ page: page })
-                    }
-                    }>→</Button>
-                </div>
+                <Pagination page={page} total_pages={data.data.total_pages} handleSetSearchParams={handleSetSearchParams} />
             )}
         </>
     )
