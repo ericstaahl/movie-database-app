416be49082834e5f60bf68625a982b7b8709a48a
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-27 10:11:37 +0200

    Changed styling on homepage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
 import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
 import RecentlyViewedMovies from "../components/RecentlyViewedMovies"
+import Row from "react-bootstrap/Row"
 
 const HomePage = () => {
   return (
-    <Container className="d-flex justify-content-center flex-wrap align-items-center min-vh-100">
-      <h2>Welcome to the Movie Database</h2>
-      <RecentlyViewedMovies />
+    <Container className="">
+      <Row className="d-flex justify-content-center align-items-cente text-center">
+        <Col className="mt-5" xs={12}>
+          <h2>Welcome to the Movie Database</h2>
+        </Col>
+        <Col className="mt-5" xs={12}>
+          <RecentlyViewedMovies />
+        </Col>
+      </Row>
     </Container>
   )
 }
