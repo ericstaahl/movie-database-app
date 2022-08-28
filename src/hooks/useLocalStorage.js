ef0a8183e8647c105e45ac8dc0fcf4b6431b7115
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-08-28 11:09:02 +0200

    Added comments. setValue now only does something if the value recieved is truthy,

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,17 +2,21 @@ import { useState } from 'react'
 
 const useLocalStorage = (property, initialValue) => {
 
+    // Needs to be stateful for it to work in TrendingPage
     const [savedValue, setSavedValue] = useState(() => localStorage.getItem(property)
         ? JSON.parse(localStorage.getItem(property))
         : null
     )
 
     if (!savedValue) {
+        // Set the initial value if no value was found in Local Storage
         localStorage.setItem(property, JSON.stringify(initialValue))
     }
 
     const setValue = (valueToBeSet) => {
-        if (savedValue) {
+        // Set the value of the item in LocalStorage to the value recieved (if truthy).
+        // Also set state. 
+        if (valueToBeSet) {
             console.log("Value to be set: ", valueToBeSet)
             setSavedValue(valueToBeSet)
             localStorage.setItem(property, JSON.stringify(savedValue))
